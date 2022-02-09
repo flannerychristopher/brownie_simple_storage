457b8c623b7dc293075d770652cfd65cbc41953f
flannerychristopher <chris@DESKTOP-KKS8SL8.localdomain>
2022-02-08 17:29:23 -0700

    get_account helper for dev

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,18 +1,23 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 import os
 
 
 def deploy_simple_storage():
-    # account = accounts.add(config["wallets"]["from_key"])
-    account = accounts[0]
+    account = get_account()
     print(account)
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
     print(stored_value)
     transaction = simple_storage.store(15, {"from": account})
+    print(transaction)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
 
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_simple_storage()

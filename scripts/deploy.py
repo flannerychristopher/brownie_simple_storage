be236f36db50bec746070e6859c4f7ca7b00ec24
flannerychristopher <chris@DESKTOP-KKS8SL8.localdomain>
2022-02-08 17:12:47 -0700

    add setting a value to the deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import accounts, config
+from brownie import accounts, config, SimpleStorage
 import os
 
 
@@ -6,6 +6,12 @@ def deploy_simple_storage():
     # account = accounts.add(config["wallets"]["from_key"])
     account = accounts[0]
     print(account)
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
 def main():
